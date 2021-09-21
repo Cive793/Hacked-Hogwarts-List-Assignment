8cfbf9a23267ee745daf76829e764f55c70194d4
peterstastny <peter.stastny7@gmail.com>
2021-09-22 00:51:49 +0200

    styling done

--- script.js
+++ script.js
@@ -1,28 +1,68 @@
 "use strict";
 
-//search button
+window.addEventListener("DOMContentLoaded", start);
 
-document.querySelector(".searchButton").addEventListener("click", buttonDropdown);
+let allStudents = [];
 
-document.querySelector("#searchInput").addEventListener("keyup", filterFunction);
+const Student = {
+  image: "",
+  firstName: "",
+  middleName: "",
+  lastName: "",
+  nickName: "",
+  gender: "",
+  house: "",
+  bloodStatus: "",
+  prefect: false,
+  inquisitorial: false,
+  quidditch: false,
+  expelled: false,
+};
 
+function start() {
+  console.log("start");
+
+  //search button
+  document.querySelector(".searchButton").addEventListener("click", buttonDropdown);
+  document.querySelector("#searchInput").addEventListener("keyup", getSearchFilterChoice);
+  //search button
+
+  getSearchFilterChoice();
+  loadJson();
+}
+
+//search button dropdown
 function buttonDropdown() {
   document.getElementById("searchDropdown").classList.toggle("show");
 }
 
-/* function filterFunction() {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown");
-  a = div.getElementsByTagName("a");
-  for (i = 0; i < a.length; i++) {
-    txtValue = a[i].textContent || a[i].innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      a[i].style.display = "";
-    } else {
-      a[i].style.display = "none";
-    }
-  }
+//search button filter choice
+function getSearchFilterChoice() {
+  //document.querySelectorAll(".searchFilterChoice").forEach((button) => button.addEventListener("click", selectFilter));
 }
- */
+
+function loadJson() {
+  fetch("https://petlatkea.dk/2021/hogwarts/students.json")
+    .then((response) => response.json())
+    .then((jsonData) => {
+      prepareObjects(jsonData);
+    });
+}
+
+function prepareObjects(jsonData) {
+  allStudents = jsonData.map(prepareObject);
+  console.log(allStudents);
+
+  buildList();
+}
+
+function prepareObject() {
+  const student = Object.create(Student);
+  student.img = createImage(student.fullname);
+  student.firstName = createFirstName(student.fullname);
+  student.firstName = createMiddleName(student.fullname);
+  student.firstName = createLastName(student.fullname);
+  student.firstName = createNickName(student.fullname);
+  student.gender = createFirstName(student.fullname);
+  student.firstName = Student.gender;
+}
